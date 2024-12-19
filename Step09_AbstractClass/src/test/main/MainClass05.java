cba3a7320585777018d0c3b88acf91b28e855ced
KWAK RAEHUI <glasofk12345@naver.com>
2024-12-19 16:59:10 +0900

    Step09 MainClass05에 주석 추가함

--- Step09_AbstractClass/src/test/main/MainClass05.java
+++ Step09_AbstractClass/src/test/main/MainClass05.java
@@ -4,7 +4,7 @@ import test.mypac.Weapon;
 
 public class MainClass05 {
 	public static void main(String[] args) {
-
+		//익명의 inner class를 이용해서 Weapon type 의 참조값을 얻어내서 변수에 담은 다음
 //		Weapon w1 = new Weapon() {//class ? extends Weapon{}
 			
 			Weapon w1=new Weapon() {
@@ -15,9 +15,10 @@ public class MainClass05 {
 					System.out.println("공격하기");
 				}
 			};
-		
+			//메소드 호출하면서 전달하기
 			useWeapon(w1);
 			
+			//메소드 호출하면서 즉석에서 Weapon type 의 참조값을 얻어내서 전달하기
 			useWeapon(new Weapon() {
 				
 				@Override
