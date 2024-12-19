403a49fe0e4bf85125b13295a778e0dc9014da64
KWAK RAEHUI <glasofk12345@naver.com>
2024-12-19 17:08:16 +0900

    MyWeapon2를 git에 올릴거야!

---
+++ Step09_AbstractClass/src/test/mypac/MyWeapon2.java
@@ -0,0 +1,10 @@
+package test.mypac;
+
+public class MyWeapon2 extends Weapon {
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("git에 추가할거야~");
+	}
+
+}
